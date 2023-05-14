5b95bbee832a3a078b20744a7cdde25281cba6ae
Dejvi Hoxha <dejvihoxha98@gmail.com>
2023-05-15 01:17:20 +0200

    first commit

---
+++ style.js
@@ -0,0 +1,10 @@
+function LogOut (element){
+    element.innerText = "Logout";
+}
+
+function Remove (element){
+    element.remove();
+}
+function Alarm(element){
+    element.Alarm('Ninja was liked')
+}
